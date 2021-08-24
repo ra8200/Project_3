b8a6dfde1e89df274bddd7e2dc4d5d6690f4bd56
ra8200 <ra8200@hotmail.com>
2021-08-23 21:40:30 -0400

    login screen

---
+++ rhythmr/src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import InputInfo from "../components/InputInfo";
+
+function login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <InputInfo />
+    </div>
+  );
+}
+
+export default login;
