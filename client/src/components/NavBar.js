c67699a7667d447d916aaec9e342019a23ad8946
ra8200 <ra8200@hotmail.com>
2021-08-25 19:30:47 -0400

    pulling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,12 +4,21 @@ import { SocialIcon } from "react-social-icons";
 
 export default function NavBar () {
     return (
-        <header >
-            <div>
-                <nav>
+        <header className="bg-green-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
                     <NavLink to="/"
+                    exact
+                    activeClassName="text-white"
+                    className="inline-flex items center py-6 px-3 mr-4 text-white text-4xl font-bold styleMe tracking-widest"
                     >
-
+                       RhythmR     
+                    </NavLink>
+                    <NavLink to="/project"
+                    className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-black"
+                    activeClassName="bg-blue-800"
+                    >
+                        Login/Sign-Up
                     </NavLink>
                 </nav>
             </div>
