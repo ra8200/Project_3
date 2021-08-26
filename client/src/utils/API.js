e653d5e6a6bbcb3a59a4a3e8e8b622a6b0482e33
ra8200 <ra8200@hotmail.com>
2021-08-26 14:39:13 -0400

    pulling

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,32 +1,4 @@
 export const getMe = (token) => {
-<<<<<<< HEAD
-    return fetch ("/api/users/me", {
-        headers: {
-            "Content-Type": "application/json",
-            authorization: `Bearer ${token}`,
-        },
-    });
-};
-export const createUser = (userData) => {
-    return fetch("/api/users", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-    });
-};
-
-export const loginUser = (userData) => {
-    return fetch("/api/users/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(userData),
-    });
-};
-=======
   return fetch("/api/users/me", {
     headers: {
       "Content-Type": "application/json",
@@ -54,4 +26,3 @@ export const loginUser = (userData) => {
     body: JSON.stringify(userData),
   });
 };
->>>>>>> 80bbbb8f9c3334d635dc0d0a264d66c361b0fc3a
