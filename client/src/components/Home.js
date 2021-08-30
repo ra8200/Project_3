583a36c9393cfd13b3c6420747c2244288ebb552
Austin Baxley <ausbaxley@gmail.com>
2021-08-30 15:17:24 -0400

    pulldown

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,18 +7,20 @@ import LessonMenu from "../components/Lessons/LessonMenu";
 export default function Home() {
   return (
     <main>
-      <img
-        src={image}
-        alt="Home"
-        className="absolute object-cover w-full h-full"
-      />
-      <section className="relative flex justify-center min-h-screen pt-12 lg:64 px-8">
-        <div>
-          <h1 className="text-6xl text-white font-bold styleMe leading-none lg:leading-snug home-name">
-            Welcome to RhythmR
-          </h1>
-        </div>
-      </section>
+      <body className="bg-gray-700">
+        <img
+          src={image}
+          alt="Home"
+          className="absolute object-cover w-full h-full"
+        />
+        <section className="relative flex justify-center min-h-screen pt-12 lg:64 px-8">
+          <div>
+            {/* <h1 className="text-2xl text-white font-bold styleMe leading-none lg:leading-snug home-name">
+              Welcome to RhythmR
+            </h1> */}
+          </div>
+        </section>
+      </body>
     </main>
   );
 }
