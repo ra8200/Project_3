71e43094c8c358ec8f31af5a1c6d3ffa7ebf7fb4
ra8200 <ra8200@hotmail.com>
2021-08-30 14:41:30 -0400

    Merge branch 'main' of https://github.com/ra8200/Project_3 into main

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,8 @@
 import React from "react";
-import image from "../rhythmrBg.jpg";
-// import Piano from "../components/Piano";
-// import Toolbox from "./toolbox";
-import LessonMenu from "../components/Lessons/LessonMenu";
 
 export default function Home() {
   return (
     <main>
-      {/* <img
-        src={image}
-        alt="Home"
-        className="absolute object-cover w-full h-full"
-      /> */}
       <section className="relative flex justify-center min-h-screen pt-12 lg:64 px-8">
         <div>
           <h1 className="text-6xl text-white font-bold styleMe leading-none lg:leading-snug home-name">
